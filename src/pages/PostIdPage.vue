ed0d5fb8f99601313e6940972e4533cf492dff74
Lexandrina90 <Lexandrina90@gmail.com>
2023-10-20 22:03:11 +0300

    Add Edit function and saved res to localStorage

--- src/pages/PostIdPage.vue
+++ src/pages/PostIdPage.vue
@@ -9,23 +9,40 @@
     <div v-if="post" class="post-content">
       <div class="post__title">
         <h1>Title:</h1>
-        <div v-if="!isEdit">{{ post.title }}</div>
-        <input v-else v-model="editedPost.title" @blur="saveAndExitEditMode" />
+        <div
+          :contenteditable="isEdit ? 'true' : 'false'"
+          @blur="saveAndExitEditMode"
+          @input="editField($event, 'title')"
+          style="outline: none"
+          :class="{ 'editable': isEdit }"
+        >
+          {{ post.title }}
+        </div>
       </div>
 
       <div class="post__description">
         <h2>Description:</h2>
-        <div v-if="!isEdit">{{ post.body }}</div>
-        <input v-else v-model="editedPost.body" @blur="saveAndExitEditMode" />
+        <div
+          :contenteditable="isEdit ? 'true' : 'false'"
+          @blur="saveAndExitEditMode"
+          @input="editField($event, 'body')"
+          style="outline: none"
+          :class="{ 'editable': isEdit }"
+        >
+          {{ post.body }}
+        </div>
       </div>
 
       <div class="post__text">
         <h2>Text:</h2>
-        <div v-if="!isEdit">
+        <div
+          :contenteditable="isEdit ? 'true' : 'false'"
+          @blur="saveAndExitEditMode"
+          @input="editField($event, 'text')"
+          style="outline: none"
+          :class="{ 'editable': isEdit }"
+        >
           {{ post.text }}
-        </div>
-        <div v-else contenteditable="true" @blur="saveAndExitEditMode" style="outline: none;">
-          {{ editedPost.text }}
         </div>
         <span class="post__date">
           <icon-date />
@@ -110,14 +127,27 @@ export default {
     },
     editPost() {
       this.isEdit = true;
-      this.editedPost.title = this.post.title;
-      this.editedPost.body = this.post.body;
-      this.editedPost.text = this.post.text;
+    },
+    editField(event, fieldName) {
+      this.editedPost[fieldName] = event.target.innerText;
+      console.log(this.post[fieldName]);
+      console.log(this.editedPost[fieldName]);
+      // if (this.post[fieldName] !== editedPost[fieldName]) {
+      //   this.isUpdate = true;
+      // }
+      // console.log(this.isUpdate)
+
+      const savedPosts = localStorage.getItem("posts");
+      const posts = JSON.parse(savedPosts);
+      const postId = Number(this.$route.params.id);
+      const postIndex = posts.findIndex((post) => post.id === postId);
+
+      if (postIndex !== -1) {
+        posts[postIndex][fieldName] = this.editedPost[fieldName];
+        localStorage.setItem("posts", JSON.stringify(posts));
+      }
     },
     saveAndExitEditMode() {
-      this.post.title = this.editedPost.title;
-      this.post.body = this.editedPost.body;
-      this.post.text = this.editedPost.text;
       this.isEdit = false;
     },
   },
@@ -162,5 +192,9 @@ input {
   font-size: inherit;
   font-family: inherit;
   white-space: normal;
+}
+.editable {
+  border-bottom: 1px solid teal;
+  padding: 5px;
 }
 </style>
